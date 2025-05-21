1efa410fe4d43a1ff28a6ea306f22de1918e1a46
Tetro48 <76738929+Tetro48@users.noreply.github.com>
2025-05-21 22:00:42 +0700

    v0.1.2: skeletons now melee you only if they've shot and you're too close

--- src/main/java/net/tetro48/tweaks/mixin/EntitySkeletonMixin.java
+++ src/main/java/net/tetro48/tweaks/mixin/EntitySkeletonMixin.java
@@ -8,36 +8,56 @@ import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(EntitySkeleton.class)
 public abstract class EntitySkeletonMixin extends EntityMob {
 	@Shadow private EntityAIAttackOnCollide aiMeleeAttack;
 	@Shadow private SkeletonArrowAttackBehavior aiRangedAttack;
 
-	@Shadow public abstract void setCombatTask();
-
 	@Unique private boolean isTargetEntityInRange = false;
 	@Unique private boolean wasTargetEntityInRange = false;
+	@Unique private boolean hasSkeletonShot = false;
+	@Unique private boolean isSkeletonMelee = false;
+
+	//this is in ticks
+	@Unique private int switchBackGracePeriod = 0;
 
 	public EntitySkeletonMixin(World par1World) {
 		super(par1World);
 	}
 
+	@Inject(method = "attackEntityWithRangedAttack", at = @At("HEAD"))
+	private void markRangedShot(EntityLivingBase target, float fDamageModifier, CallbackInfo ci) {
+		System.out.println("skele shot");
+		hasSkeletonShot = true;
+	}
+
 	@Inject(method = "onLivingUpdate", at = @At("RETURN"))
 	private void meleeWhenUpClose(CallbackInfo ci) {
-		if (getAttackTarget() != null) {
+		EntityLivingBase target = getAttackTarget();
+		if (target != null) {
 			isTargetEntityInRange = getAttackTarget().getDistanceToEntity(this) < 1.25f;
 		}
-		if (isTargetEntityInRange && !wasTargetEntityInRange) {
-			this.tasks.removeTask(this.aiMeleeAttack);
-			this.tasks.removeTask(this.aiRangedAttack);
-			this.tasks.addTask(4, this.aiMeleeAttack);
+		if (switchBackGracePeriod > 0) {
+			switchBackGracePeriod--;
 		}
-		else if (!isTargetEntityInRange) {
+		if (!isTargetEntityInRange) {
+			hasSkeletonShot = false;
+		}
+		if (isTargetEntityInRange && !isSkeletonMelee && hasSkeletonShot) {
+			this.tasks.removeTask(this.aiRangedAttack);
+			this.tasks.addTask(3, this.aiMeleeAttack);
+			isSkeletonMelee = true;
+			hasSkeletonShot = false;
+			switchBackGracePeriod = 60;
+		}
+		else if (!isTargetEntityInRange && isSkeletonMelee && switchBackGracePeriod <= 0) {
 			ItemStack heldStack = this.getHeldItem();
 			if (heldStack != null && heldStack.itemID == Item.bow.itemID) {
 				this.tasks.removeTask(this.aiMeleeAttack);
 				this.tasks.addTask(4, this.aiRangedAttack);
+				isSkeletonMelee = false;
 			}
 		}
 		wasTargetEntityInRange = isTargetEntityInRange;
