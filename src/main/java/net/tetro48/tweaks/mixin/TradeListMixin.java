f2f6f6b96e743ddedc2b3066bf680a363217d743
Tetro48 <76738929+Tetro48@users.noreply.github.com>
2025-10-02 11:29:13 +0700

    fix this buggy code

--- src/main/java/net/tetro48/tweaks/mixin/TradeListMixin.java
+++ src/main/java/net/tetro48/tweaks/mixin/TradeListMixin.java
@@ -11,26 +11,26 @@ public abstract class TradeListMixin {
 	@ModifyArgs(method = "addBlacksmithTrades", remap = false, at = @At(value = "INVOKE", target = "Lbtw/entity/mob/villager/trade/TradeProvider$SecondaryCostStep;secondaryEmeraldCost(II)Lbtw/entity/mob/villager/trade/TradeProvider$FinalStep;"))
 	private static void reduceBlacksmithScrollCost(Args args) {
 		args.set(0, 32);
-		args.set(0, 48);
+		args.set(1, 48);
 	}
 	@ModifyArgs(method = "addFarmerTrades", remap = false, at = @At(value = "INVOKE", target = "Lbtw/entity/mob/villager/trade/TradeProvider$SecondaryCostStep;secondaryEmeraldCost(II)Lbtw/entity/mob/villager/trade/TradeProvider$FinalStep;"))
 	private static void reduceFarmerScrollCost(Args args) {
 		args.set(0, 32);
-		args.set(0, 48);
+		args.set(1, 48);
 	}
 	@ModifyArgs(method = "addLibrarianTrades", remap = false, at = @At(value = "INVOKE", target = "Lbtw/entity/mob/villager/trade/TradeProvider$SecondaryCostStep;secondaryEmeraldCost(II)Lbtw/entity/mob/villager/trade/TradeProvider$FinalStep;"))
 	private static void reduceLibrarianScrollCost(Args args) {
 		args.set(0, 32);
-		args.set(0, 48);
+		args.set(1, 48);
 	}
 	@ModifyArgs(method = "addPriestTrades", remap = false, at = @At(value = "INVOKE", ordinal = 16, target = "Lbtw/entity/mob/villager/trade/TradeProvider$SecondaryCostStep;secondaryEmeraldCost(II)Lbtw/entity/mob/villager/trade/TradeProvider$FinalStep;"))
 	private static void reducePriestScrollCost(Args args) {
 		args.set(0, 32);
-		args.set(0, 48);
+		args.set(1, 48);
 	}
 	@ModifyArgs(method = "addButcherTrades", remap = false, at = @At(value = "INVOKE", ordinal = 1, target = "Lbtw/entity/mob/villager/trade/TradeProvider$SecondaryCostStep;secondaryEmeraldCost(II)Lbtw/entity/mob/villager/trade/TradeProvider$FinalStep;"))
 	private static void reduceButcherScrollCost(Args args) {
 		args.set(0, 32);
-		args.set(0, 48);
+		args.set(1, 48);
 	}
 }
